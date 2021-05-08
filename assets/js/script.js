7dc9dd69beb143258b6520d641f6357e3d5d52a8
Joseph Sumis <Jsumis@comcast.net>
2021-05-08 01:31:09 -0500

    added eventListener to 8am button and function to save textBox to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,27 @@
+// Displays current date and time using moment
+var now = moment().format("ddd MMM Do, YYYY");
+$("#currentDay").text(now);
+
+var saveButton = document.getElementById("save8am");
+saveButton.addEventListener("click", function (save8amText) {
+    save8amText.preventDefault();
+    var textBox8am = document.getElementById("textBox8am");
+    localStorage.setItem("8amContents", textBox8am.value);
+}
+)
+localStorage.getItem
+
+
 /* Psuedo-code
 
 Table needs three columns
 1st column is hourly starting 9Am-5PM There are 9 columns
 Rows are color coded based on actual time page is loaded (light gray for past,red for present, and green for future) using moment widget
-middle column data entry click to enter an even
+middle column data entry click to enter an event
 right column is a save button that saves data to local storage
 when page is refreshed, data stays (event.preventDefault();)
 
 
 
 
-
-
-
-
 */
-// Displays current date and time using moment
-var now = moment().format("ddd MMM Do, YYYY");
-$("#currentDay").text(now);
